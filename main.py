a0ab4b40358457fab8fde168e0d96d2432901cee
CodeK <isaacmcampos101@gmail.com>
2026-03-29 18:08:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 tabuleiro = [["   " for j in range(8)]for i in range(8)]
 pecas_brancas = [' T ',' C ','>B ',' D ',' R ',' B<',' C ',' T ',' P ']
 pecas_pretas = [' t ',' c ','>b ',' d ',' r ',' b<',' c ',' t ',' p ']
@@ -29,46 +30,22 @@ visualizacao()
 
 while True:
     if jogador % 2 == 0:
+        while True:
+            l = int(input("Qual a linha da peça que deseja mexer:")) -1
+            c = int(input("Qual a coluna da peça que deseja mexer:")) -1
+            destinoL = int(input("Qual a linha que deseja enviar a peça:")) -1
+            destinoC = int(input("Qual a coluna que deseja enviar a peça:")) -1
 
-        l = input("Qual a linha da peça que deseja mexer:")
-        c = input("Qual a coluna da peça que deseja mexer:")
-        destinoL = input("Qual a linha que deseja enviar a peça:")
-        destinoC = input("Qual a coluna que deseja enviar a peça:")
+            if -1 < l < 8 and -1 < c < 8 and -1 < destinoL < 8 and -1 < destinoC < 8:
+                break
+
+        condicao = Gerenciador(tabuleiro,l,c,destinoL,destinoC)
+        condicao.adiministracao()
 
         if (condicao) == 1:
             tabuleiro[destinoL][destinoC] = tabuleiro[l][c]
             tabuleiro[l][c] = "   "
             jogador = + 1
-            
+
         else:
             print("Jogada invalida")
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
